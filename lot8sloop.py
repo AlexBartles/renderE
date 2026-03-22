806d72fe67533ecd066ac133c44858b0c2ff739f
LeWolfYT <ciblox3@gmail.com>
2026-03-22 15:46:12 -0400

    lot8s scheduler revamp stuff

--- lot8sloop.py
+++ lot8sloop.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 from tkinter import messagebox
 import subprocess
 import time
@@ -6,19 +7,13 @@ import threading as th
 
 root = tk.Tk()
 running = False
-global encodEing
 encodEing = False
-global reallot8stype
 reallot8stype = "D" # Fallback in case something decides to break
 stop_event = th.Event()
 stop_encodE_event = th.Event()
 
 # Set the window properties
-root.title("Lot8s Scheduler")
-root.configure(background="#ADD8E6")
-root.minsize(400, 300)
-root.maxsize(1000, 800)
-root.geometry("700x500")
+root.title("Lot8s Looper")
 
 # Manual debugging toggle
 DEBUG = False
@@ -27,32 +22,34 @@ DEBUG = False
 if DEBUG == True:
     print("Debugging mode enabled.")
 
+preroll = tk.IntVar(root, 8)
+
 # Lot8s run function
 def run():
     while running:
         # Here is where the run the actual lot8s commands, FINALLY!!!!!!!!!
         if DEBUG == True:
-            print("Running lot8s with flavor: {}".format(reallot8stype))
+            print("Running presentation with flavor: {}".format(reallot8stype))
         subprocess.run(["python3", "load.py", "local", reallot8stype])
-        time.sleep(3)
+        time.sleep(preroll.get())
         subprocess.run(["python3", "run.py", "local"])
         if reallot8stype in ["D", "E", "S"]:
             if stop_event.wait(60):
-                break;
+                break
         if reallot8stype in ["K", "O"]:
             if stop_event.wait(90):
-                break;
+                break
         if reallot8stype in ["N", "L", "M"]:
             if stop_event.wait(90):
-                break;
+                break
 
 def encodE():
     while encodEing:
         if DEBUG == True:
-            print("EncodEing starts now!")
+            print("Running encodE!")
         subprocess.run(["python3", "encodE.py"])
         if stop_encodE_event.wait(600):
-            break;
+            break
 
 # Command for start button
 def startbuttoncommand():
@@ -60,11 +57,11 @@ def startbuttoncommand():
     global running
     reallot8stype = value_inside.get()[0]
     if value_inside.get() == "Select an Option":
-        tk.messagebox.showwarning("Please select an option!", "Please select an option for what lot8s to run! Defaulting to D.")
+        messagebox.showwarning("Please select an option!", "Please select an option for what presentation to run! Defaulting to D.")
         reallot8stype = "D"
     if value_inside.get()[0] not in ["D", "E", "K", "O", "N", "L", "M", "S"]:
         print("Lot8s selector broken. Real value is: {}".format(value_inside.get()[0]))
-        tk.messagebox.showerror("Unknown lot8s type", "Welp, it seems something has broken with the lot8s type selector!")
+        messagebox.showerror("Unknown presentation type", "Welp, it seems something has broken with the presentation type selector!")
     else:
         if running == False:
             running = True
@@ -72,7 +69,7 @@ def startbuttoncommand():
             run_th.start()
             stop_event.clear()
         else:
-            tk.messagebox.showerror("Already running lot8s!", "The lot8s are already running, go check renderE!")
+            messagebox.showerror("Already running presentation!", "The presentation is already running, go check renderE!")
 
 # Command for stop button
 def stopbuttoncommand():
@@ -81,7 +78,7 @@ def stopbuttoncommand():
         running = False
         stop_event.set()
     else:
-        tk.messagebox.showerror("Lot8s aren't running!", "Not running lot8s, start them first!")
+        messagebox.showerror("Presentation isn't running!", "Not running a presentation, start it first!")
 
 def startencodEcommand():
     global encodEing
@@ -91,7 +88,7 @@ def startencodEcommand():
         encodE_th.start()
         stop_event.clear()
     else:
-        tk.messagebox.showerror("Already running encodE!", "Already encodEing!")
+        messagebox.showerror("Already running encodE!", "Already encodEing!")
 
 def stopencodEcommand():
     global encodEing
@@ -99,42 +96,54 @@ def stopencodEcommand():
         encodEing = False
         stop_encodE_event.set()
     else:
-        tk.messagebox.showerror("EncodE isnt running!", "Not encodEing, start it first!")
+        messagebox.showerror("encodE isnt running!", "Not encodEing, start it first!")
 
 # All lot8s flavors
-lot8s_options = ["D - 60 seconds", "E - 60 seconds", "K - 90 seconds", "O - 90 seconds", "N - 120 seconds", "L - 120 seconds", "M - 120 seconds", "S - squeezeback"]
+lot8s_options = ["D - 60 seconds", "E - 60 seconds", "K - 90 seconds", "O - 90 seconds", "N - 120 seconds", "L - 120 seconds", "M - 120 seconds", "S - Squeezeback"]
 
 # Set blank option string
 value_inside = tk.StringVar(root)
 value_inside.set("Select an Option")
 
-# Create a title (this is already annoying)
-tk.Label(root, text="Lot8s Scheduler", background="#ADD8E6", font=("Helvetica", 16, "bold")).pack(pady=20)
+# you are cordially invited to have a huge slice of my style
+style = ttk.Style(root)
 
-# Ask what type of lot8s to run
-lot8stype = tk.Label(root, text="What flavor of lot8s do you want to run?", font=("Helvetica", 12), background="#ADD8E6")
-lot8stype.pack(pady=20)
+style.configure("my.TLabel", font=("Helvetica", 20, "bold"))
+
+# Create a title (this is already annoying)
+ttk.Label(root, text="Lot8s Looper", style="my.TLabel").pack(pady=(15, 0), padx=10)
+ttk.Label(root, text="by AlexBartles").pack(pady=(0, 10))
+
+# Ask to choose a flavor
+lot8stype = ttk.Label(root, text="Choose a flavor below")
+lot8stype.pack(pady=5)
 
 # Further questioning continues
-question_menu = tk.OptionMenu(root, value_inside, *lot8s_options)
-question_menu.config(bg="#ADD8E6")
-question_menu["menu"].config(bg="#ADD8E6")
-question_menu.pack(pady=20)
+question_menu = ttk.OptionMenu(root, value_inside, *lot8s_options)
+question_menu.pack(pady=5)
+
+# Preroll time
+lot8stype = ttk.Label(root, text="Preroll time (seconds)")
+lot8stype.pack(pady=5)
+
+preroll_box = ttk.Spinbox(root, from_=1, to=10, textvariable=preroll)
+preroll_box.set(8)
+preroll_box.pack(pady=5, padx=10)
 
 # Create start button
-startbutton = tk.Button(root, text="Start lot8s with flavor selected above", font=("Helvetica", 12), background="#ADD8E6", command=startbuttoncommand)
-startbutton.pack(pady=20)
+startbutton = ttk.Button(root, text="Start presentation", style="my.TButton", command=startbuttoncommand)
+startbutton.pack(pady=5)
 
 # Create stop button
-stopbutton = tk.Button(root, text="Stop lot8s", font=("Helvetica", 12), background="#ADD8E6", command=stopbuttoncommand)
-stopbutton.pack(pady=20)
+stopbutton = ttk.Button(root, text="Stop presentation", style="my.TButton", command=stopbuttoncommand)
+stopbutton.pack(pady=5)
 
 # Create start encodE button
-startencodEbutton = tk.Button(root, text="Start encodEing", font=("Helvetica", 12), background="#ADD8E6", command=startencodEcommand)
-startencodEbutton.pack(pady=20)
+startencodEbutton = ttk.Button(root, text="Start encodEing", style="my.TButton", command=startencodEcommand)
+startencodEbutton.pack(pady=5)
 
 # Create stop encodE button
-stopencodEbutton = tk.Button(root, text="Stop encodEing", font=("Helvetica", 12), background="#ADD8E6", command=stopencodEcommand)
-stopencodEbutton.pack(pady=20)
+stopencodEbutton = ttk.Button(root, text="Stop encodEing", style="my.TButton", command=stopencodEcommand)
+stopencodEbutton.pack(pady=(5, 15))
 
 root.mainloop()
