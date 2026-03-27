89f8354d66045aa4611620bb8e4501ed9c62efa3
LeWolfYT <ciblox3@gmail.com>
2026-03-27 12:59:46 -0400

    can i have some money? money? money? *FOG HORN SOUND EFFECT* WHAT THE BARNACLES?

--- nethandler.py
+++ nethandler.py
@@ -1,6 +1,7 @@
 import requests as r
 import os
 import json
+from pathlib import PurePath
 
 servers = [
     "https://archive.lewolfyt.cc/PerrisLive/",
@@ -8,15 +9,22 @@ servers = [
 #    "https://archive.lewolfyt.cc/WxScanLive/"
 ]
 
+def newjoin(*args):
+    pp = PurePath(*args).as_posix()
+    jp = os.path.join(*args)
+    if jp.endswith("/") or jp.endswith("\\") and not (pp.endswith("/")):
+        pp = pp + "/"
+    return pp
+
 try:
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "servers.json")) as f:
+    with open(newjoin(os.path.dirname(os.path.abspath(__file__)), "servers.json")) as f:
         servers = json.loads(f.read())[1:]
 except:
     import traceback
     traceback.print_exc()
     pass
 
-temp = os.path.join(
+temp = newjoin(
     os.path.dirname(os.path.abspath(__file__)),
     "net"
 )
@@ -32,15 +40,15 @@ def requestNetAsset(path : str, extensions, check=False):
     all = fonts + gfx + aud
     emap = {"font": fonts, "gfx": gfx, "audio": aud, "all": all}
     for ex in emap[extensions]:
-        out = os.path.join(temp, path.strip("/"))+"."+ex
+        out = newjoin(temp, path.strip("/"))+"."+ex
         if os.path.exists(out):
             return out
     if check:
         return
     for ex in emap[extensions]:
-        out = os.path.join(temp, path.strip("/"))+"."+ex
+        out = newjoin(temp, path.strip("/"))+"."+ex
         for server in servers:
-            spath = os.path.join(server, path.strip("/"))+"."+ex
+            spath = newjoin(server, path.strip("/"))+"."+ex
             print(spath)
             if r.head(spath).ok:
                 os.makedirs(os.path.dirname(out), exist_ok=True)
@@ -51,13 +59,13 @@ def requestNetAsset(path : str, extensions, check=False):
     return None
 
 def requestNetAssetExt(path : str, ext=None, check=False):
-    out = os.path.join(temp, path.strip("/"))+("."+ext if ext else "")
+    out = newjoin(temp, path.strip("/"))+("."+ext if ext else "")
     if os.path.exists(out):
         return out
     if check:
         return
     for server in servers:
-        spath = os.path.join(server, path.strip("/"))+("."+ext if ext else "")
+        spath = newjoin(server, path.strip("/"))+("."+ext if ext else "")
         print(spath)
         if r.head(spath).ok:
             os.makedirs(os.path.dirname(out), exist_ok=True)
